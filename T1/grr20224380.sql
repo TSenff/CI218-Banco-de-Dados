2713e9cd8b82d732adb27e2ca9b3d5098318711a
Senff <tcps22@inf.ufpr.br>
2023-09-13 17:22:10 -0300

    [ADD] Base do trabalho 1

---
+++ T1/grr20224380.sql
@@ -0,0 +1,74 @@
+/*
+-- Questão 1 
+SELECT id, populacao
+FROM cidade;
+
+-- Questão 2 
+SELECT *
+FROM cidade
+WHERE populacao<130000; 
+
+-- Questão 3
+SELECT id, populacao
+FROM cidade AS c, cidademar AS m 
+WHERE c.id=m.cidade AND c.populacao<130000; 
+
+-- Questão 4
+SELECT id, populacao
+FROM cidade AS c JOIN cidademar AS m ON c.id=m.cidade
+WHERE c.populacao<130000; 
+
+-- Questão 5
+SELECT p.nome
+FROM pais AS p 
+JOIN paiscontinente AS pc ON pc.pais=p.codigo
+WHERE pc.continente='Europe'
+INTERSECT
+SELECT p.nome
+FROM paisreligiao AS pr 
+JOIN pais AS p ON p.codigo=pr.pais
+*/
+-- Questão 6
+--A :={Paises e linguas}
+--B :={Russo e Armenio}
+--A/B := {Paises que falam russo e armenio}
+
+SELECT *
+FROM paislingua AS pl
+
+SELECT *
+FROM lingua AS l
+WHERE l.lingua='Armenian' OR l.lingua='Russian';
+
+/*
+-- Questão 7
+SELECT c.id, c.pais
+FROM cidademar AS m
+JOIN cidaderio AS r ON r.cidade=m.cidade
+JOIN cidade    AS c ON r.cidade=c.id
+WHERE c.pais IN (
+    SELECT pc.pais 
+    FROM paiscontinente AS pc
+    WHERE pc.continente='Asia'
+)
+
+-- Questão 8
+SELECT c.id
+FROM cidade AS c
+EXCEPT
+SELECT m.cidade
+FROM cidademar AS m; 
+
+-- Questão 9
+SELECT pc.continente, SUM(p.populacao) AS populacao
+FROM paiscontinente AS pc 
+JOIN pais AS p ON p.codigo=pc.pais
+GROUP BY pc.continente;
+
+-- Questão 10
+SELECT pc.continente, SUM(p.populacao) AS populacao, MAX(p.pib) AS pibMax, MIN(p.pib) AS pibMin
+FROM paiscontinente AS pc 
+JOIN pais AS p ON p.codigo=pc.pais
+GROUP BY pc.continente
+HAVING  MIN(p.area) < MIN(p.pib);
+*/
